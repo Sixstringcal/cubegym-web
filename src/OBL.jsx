3ec70edf3baae1b2713b8442a5ab413e4227b3bd
Calvin Nielson <sixstringcal@gmail.com>
2021-06-14 12:00:15 -0600

    complete images

--- src/OBL.jsx
+++ src/OBL.jsx
@@ -5,66 +5,131 @@ export default function OBL() {
 
     return (
         <div>
-            <svg width="100" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 98"><defs>
+            <div>
+                <svg width="100" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 98"><defs>
 
-            </defs>
-                <polygon
-                    id="1"
-                    points="0,35 0,0 35,0 49,49"
-                    fill="white"
-                    stroke="gray"
-                    stroke-width="2"
-                />
-                <polygon
-                    id="2"
-                    points="0,63 0,35 49,49"
-                    fill="white"
-                    stroke="gray"
-                    stroke-width="2"
-                />
-                <polygon
-                    id="3"
-                    points="0,63 0,98 35,98 49,49"
-                    fill="white"
-                    stroke="gray"
-                    stroke-width="2"
-                />
-                <polygon
-                    id="4"
-                    points="35,98 63,98 49,49"
-                    fill="white"
-                    stroke="gray"
-                    stroke-width="2"
-                />
-                <polygon
-                    id="5"
-                    points="98,63 98,98 63,98 49,49"
-                    fill="white"
-                    stroke="gray"
-                    stroke-width="2"
-                />
-                <polygon
-                    id="6"
-                    points="98,63 98,35 49,49"
-                    fill="white"
-                    stroke="gray"
-                    stroke-width="2"
-                />
-                <polygon
-                    id="7"
-                    points="98,35 98,0 63,0 49,49"
-                    fill="white"
-                    stroke="gray"
-                    stroke-width="2"
-                />
-                <polygon
-                    id="8"
-                    points="35,0 63,0 49,49"
-                    fill="white"
-                    stroke="gray"
-                    stroke-width="2"
-                />
-            </svg>
+                </defs>
+                    <polygon
+                        id="1"
+                        points="0,35 0,0 35,0 49,49"
+                        fill="white"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="2"
+                        points="0,63 0,35 49,49"
+                        fill="white"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="3"
+                        points="0,63 0,98 35,98 49,49"
+                        fill="white"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="4"
+                        points="35,98 63,98 49,49"
+                        fill="white"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="5"
+                        points="98,63 98,98 63,98 49,49"
+                        fill="white"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="6"
+                        points="98,63 98,35 49,49"
+                        fill="white"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="7"
+                        points="98,35 98,0 63,0 49,49"
+                        fill="white"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="8"
+                        points="35,0 63,0 49,49"
+                        fill="white"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                </svg>
+            </div>
+            <div>
+
+                <svg width="100" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 98"><defs>
+
+                </defs>
+                    <polygon
+                        id="1"
+                        points="0,35 0,0 35,0 49,49"
+                        fill="black"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="2"
+                        points="0,63 0,35 49,49"
+                        fill="black"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="3"
+                        points="0,63 0,98 35,98 49,49"
+                        fill="black"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="4"
+                        points="35,98 63,98 49,49"
+                        fill="black"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="5"
+                        points="98,63 98,98 63,98 49,49"
+                        fill="black"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="6"
+                        points="98,63 98,35 49,49"
+                        fill="black"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="7"
+                        points="98,35 98,0 63,0 49,49"
+                        fill="black"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                    <polygon
+                        id="8"
+                        points="35,0 63,0 49,49"
+                        fill="black"
+                        stroke="gray"
+                        stroke-width="2"
+                    />
+                </svg>
+            </div>
         </div>
     );
 
