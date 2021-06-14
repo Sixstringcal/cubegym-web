2b95282132f5a9c122763ccf4cd028977fc17f53
Calvin Nielson <sixstringcal@gmail.com>
2021-06-14 11:50:37 -0600

    PBL image shows now

--- src/PBL.jsx
+++ src/PBL.jsx
@@ -5,7 +5,34 @@ export default function PBL() {
 
     return (
         <div>
-            PBL
+            <svg width="100" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                viewBox="0 0 130.5 136">
+                <style type="text/css">
+
+                </style>
+                <polygon id="_1" stroke="#808080" stroke-width="2" points="15.5,54 15.5,19 50.5,19 64.5,68 " />
+                <polygon id="_2" stroke="#808080" stroke-width="2" points="15.5,82 15.5,54 64.5,68 " />
+                <polygon id="_3" stroke="#808080" stroke-width="2" points="15.5,82 15.5,117 50.5,117 64.5,68 " />
+                <polygon id="_4" stroke="#808080" stroke-width="2" points="50.5,117 78.5,117 64.5,68 " />
+                <polygon id="_5" stroke="#808080" stroke-width="2" points="113.5,82 113.5,117 78.5,117 64.5,68 " />
+                <polygon id="_6" stroke="#808080" stroke-width="2" points="113.5,82 113.5,54 64.5,68 " />
+                <polygon id="_7" stroke="#808080" stroke-width="2" points="113.5,54 113.5,19 78.5,19 64.5,68 " />
+                <polygon id="_8" stroke="#808080" stroke-width="2" points="50.5,19 78.5,19 64.5,68 " />
+                <rect x="11.5" y="19" fill="#0000FF" stroke="#808080" stroke-width="2" width="4" height="35" />
+                <rect x="11.5" y="54" fill="#0000FF" stroke="#808080" stroke-width="2" width="4" height="27.6" />
+                <rect x="11.5" y="82" fill="#0000FF" stroke="#808080" stroke-width="2" width="4" height="35" />
+                <rect x="113.5" y="19" fill="#00FF00" stroke="#808080" stroke-width="2" width="4" height="35" />
+                <rect x="113.5" y="54" fill="#00FF00" stroke="#808080" stroke-width="2" width="4" height="27.6" />
+                <rect x="113.5" y="82" fill="#00FF00" stroke="#808080" stroke-width="2" width="4" height="35" />
+                <rect x="78.5" y="15" fill="#FFA500" stroke="#808080" stroke-width="2" width="35" height="4" />
+                <rect x="50.5" y="15" fill="#FFA500" stroke="#808080" stroke-width="2" width="27.6" height="4" />
+                <rect x="15.5" y="15" fill="#FFA500" stroke="#808080" stroke-width="2" width="35" height="4" />
+                <rect x="78.5" y="117" fill="#FF0000" stroke="#808080" stroke-width="2" width="35" height="4" />
+                <rect x="50.5" y="117" fill="#FF0000" stroke="#808080" stroke-width="2" width="27.6" height="4" />
+                <rect x="15.5" y="117" fill="#FF0000" stroke="#808080" stroke-width="2" width="35" height="4" />
+
+            </svg>
+
         </div>
     );
 
