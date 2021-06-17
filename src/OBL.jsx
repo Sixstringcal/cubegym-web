470087b8e93fbdc0216caea4da03d183d256b915
Calvin Nielson <sixstringcal@gmail.com>
2021-06-17 12:52:38 -0600

    algs get pulled from firebase and show scramble and setup

--- src/OBL.jsx
+++ src/OBL.jsx
@@ -8,19 +8,30 @@ import ReactDOM from 'react-dom';
 var algs;
 var oblState = ["#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"]
 var sq1 = new Sq1();
+var currentScramble = "";
+var reversedScramble = "";
 var oblView = (<OBLImage oblState={oblState} id="obl-image" />);
 
 
 function getOBLAlgs(uLayer, dLayer) {
     fetch("http://api.cubegym.net/getOBL", {
+    //fetch("http://localhost:626/getOBL", {
+
         method: "POST",
 
     }).then((response) => response.json())
         .then((responseJSON) => {
-            algs = responseJSON.algorithms;
-            sq1.doMoves(sq1.reverseScramble(algs[2].alg));
+            algs = responseJSON.algs;
+            sq1 = new Sq1();
+            currentScramble = algs[2];
+            reversedScramble = sq1.reverseScramble(algs[2]);
+            sq1.doMoves(sq1.reverseScramble(algs[2]));
             oblState = sq1.getOBLState();
             console.log(oblState);
+
+            var setupView = (<p id='setup'>{"Setup: " + reversedScramble}</p>);
+            var algView = (<p id='alg'>{"Algorithm: " + currentScramble}</p>);
+
             var oblView = (<div id="obl-image">
                 <div>
                     <div>
@@ -154,6 +165,10 @@ function getOBLAlgs(uLayer, dLayer) {
             </div>);
             ReactDOM.render(oblView, document.getElementById('obl-image'));
 
+            ReactDOM.render(setupView, document.getElementById('setup'));
+            ReactDOM.render(algView, document.getElementById('alg'));
+
+
         });
 }
 class OBL extends Component {
@@ -162,7 +177,7 @@ class OBL extends Component {
 
     }
 
-    defaultProps = { oblState: ["#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"] }
+    defaultProps = { reversedScramble: "", currentScramble: "", oblState: ["#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"] }
 
     constructor() {
         getOBLAlgs('all', 'all');
@@ -177,138 +192,142 @@ class OBL extends Component {
 
     render() {
         return (
-            <div id="obl-image">
-
-                <div>
-                    <div>
-                        <svg width="100" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110 110" > <defs>
-
-                        </defs>
-                            < polygon
-                                id="1"
-                                points="0,35 0,0 35,0 49,49"
-                                fill={oblState[0]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="2"
-                                points="0,63 0,35 49,49"
-                                fill={oblState[1]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="3"
-                                points="0,63 0,98 35,98 49,49"
-                                fill={oblState[2]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="4"
-                                points="35,98 63,98 49,49"
-                                fill={oblState[3]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="5"
-                                points="98,63 98,98 63,98 49,49"
-                                fill={oblState[4]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="6"
-                                points="98,63 98,35 49,49"
-                                fill={oblState[5]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="7"
-                                points="98,35 98,0 63,0 49,49"
-                                fill={oblState[6]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="8"
-                                points="35,0 63,0 49,49"
-                                fill={oblState[7]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                        </svg>
-                    </div>
+            <div className='centered'>
+                <p id='setup'>{"Setup: " + reversedScramble}</p>
+                <p id='alg'>{"Algorithm: " + currentScramble}</p>
+                <div id="obl-image">
 
                     <div>
+                        <div>
+                            <svg width="100" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110 110" > <defs>
 
-                        <svg width="100" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110 110" > <defs>
+                            </defs>
+                                < polygon
+                                    id="1"
+                                    points="0,35 0,0 35,0 49,49"
+                                    fill={oblState[0]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="2"
+                                    points="0,63 0,35 49,49"
+                                    fill={oblState[1]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="3"
+                                    points="0,63 0,98 35,98 49,49"
+                                    fill={oblState[2]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="4"
+                                    points="35,98 63,98 49,49"
+                                    fill={oblState[3]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="5"
+                                    points="98,63 98,98 63,98 49,49"
+                                    fill={oblState[4]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="6"
+                                    points="98,63 98,35 49,49"
+                                    fill={oblState[5]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="7"
+                                    points="98,35 98,0 63,0 49,49"
+                                    fill={oblState[6]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="8"
+                                    points="35,0 63,0 49,49"
+                                    fill={oblState[7]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                            </svg>
+                        </div>
 
-                        </defs>
-                            < polygon
-                                id="9"
-                                points="0,35 0,0 35,0 49,49"
-                                fill={oblState[8]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="10"
-                                points="0,63 0,35 49,49"
-                                fill={oblState[9]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="11"
-                                points="0,63 0,98 35,98 49,49"
-                                fill={oblState[10]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="12"
-                                points="35,98 63,98 49,49"
-                                fill={oblState[11]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="13"
-                                points="98,63 98,98 63,98 49,49"
-                                fill={oblState[12]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="14"
-                                points="98,63 98,35 49,49"
-                                fill={oblState[13]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="15"
-                                points="98,35 98,0 63,0 49,49"
-                                fill={oblState[14]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                            <polygon
-                                id="16"
-                                points="35,0 63,0 49,49"
-                                fill={oblState[15]}
-                                stroke="gray"
-                                strokeWidth="2"
-                            />
-                        </svg>
+                        <div>
+
+                            <svg width="100" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 110 110" > <defs>
+
+                            </defs>
+                                < polygon
+                                    id="9"
+                                    points="0,35 0,0 35,0 49,49"
+                                    fill={oblState[8]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="10"
+                                    points="0,63 0,35 49,49"
+                                    fill={oblState[9]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="11"
+                                    points="0,63 0,98 35,98 49,49"
+                                    fill={oblState[10]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="12"
+                                    points="35,98 63,98 49,49"
+                                    fill={oblState[11]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="13"
+                                    points="98,63 98,98 63,98 49,49"
+                                    fill={oblState[12]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="14"
+                                    points="98,63 98,35 49,49"
+                                    fill={oblState[13]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="15"
+                                    points="98,35 98,0 63,0 49,49"
+                                    fill={oblState[14]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                                <polygon
+                                    id="16"
+                                    points="35,0 63,0 49,49"
+                                    fill={oblState[15]}
+                                    stroke="gray"
+                                    strokeWidth="2"
+                                />
+                            </svg>
+                        </div>
                     </div>
-                </div>
 
-            </div >
+                </div >
+            </div>
         );
 
     }
